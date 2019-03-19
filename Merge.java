8ede1f53320a71acf1cbf65ecf11247a240fb5a9
Ali Taha <alitaha.1187@gmail.com>
2019-03-18 22:37:55 -0400

    created wrapper funciton to perform as specified

--- Merge.java
+++ Merge.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
 public class Merge{
-    public static int[] mergesort(int[] ary){
+    public static void mergesort(int[] data){
+      data = mergesort1(data);
+    }
+    private static int[] mergesort1(int[] ary){
         if(ary.length==1)return ary;
         int[] ary1 = new int[ary.length/2];
         int[] ary2 = new int[ary.length-ary.length/2];
@@ -8,9 +11,9 @@ public class Merge{
         for(int i = 0 ; i<ary2.length ; i++)ary2[i]=ary[i+ary1.length];
         return merge(ary1,ary2);
     }
-    public static int[] merge(int[] ary1, int[] ary2){
-        int[] aryA = mergesort(ary1);
-        int[] aryB = mergesort(ary2);
+    private static int[] merge(int[] ary1, int[] ary2){
+        int[] aryA = mergesort1(ary1);
+        int[] aryB = mergesort1(ary2);
         int[] aryC = new int[aryA.length+aryB.length];
         int iA=0;
         int iB=0;
@@ -35,13 +38,12 @@ public class Merge{
               iB++;
               iC++;}
         }
-        System.out.println(Arrays.toString(aryC));
         return aryC;
         }
      public static void main(String []args){
         int[] test = new int[]{69,7,3,4,6,7,9,2,4,6,8,3,4,75,3,65,84,652};
-        int[] sorted = mergesort(test);
         System.out.println("Here's the Initial Array \n"+ Arrays.toString(test));
-        System.out.println("Here's the Sorted Array \n"+ Arrays.toString(sorted));
+        mergesort(test);
+        System.out.println("Here's the Sorted Array \n"+ Arrays.toString(mergesort1(test)));
      }
 }
