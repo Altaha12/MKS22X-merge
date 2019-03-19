8313eaaa083f201100d1e69c2f2f62606878c64a
Ali Taha <alitaha.1187@gmail.com>
2019-03-18 22:57:39 -0400

    tested with provided code, and is fast enough, close to ratios posted online

--- Merge.java
+++ Merge.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
 public class Merge{
     public static void mergesort(int[] data){
-      data = mergesort1(data);
+      int[] data21 = mergesort1(data);
+      for(int i=0;i<data.length;i++)data[i]=data21[i];
     }
     private static int[] mergesort1(int[] ary){
         if(ary.length==1)return ary;
@@ -40,10 +41,38 @@ public class Merge{
         }
         return aryC;
         }
-     public static void main(String []args){
-        int[] test = new int[]{69,7,3,4,6,7,9,2,4,6,8,3,4,75,3,65,84,652};
-        System.out.println("Here's the Initial Array \n"+ Arrays.toString(test));
-        mergesort(test);
-        System.out.println("Here's the Sorted Array \n"+ Arrays.toString(mergesort1(test)));
-     }
+     public static void main(String[]args){
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        mergesort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
+  }
+}
 }
